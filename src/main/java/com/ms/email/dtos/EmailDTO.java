a59ebbe3890a67e099b9fa0efff655b493efe0e3
Jennifer <jennifer.manhice@netline.co.mz>
2023-11-08 17:14:57 +0200

    dto validation

--- src/main/java/com/ms/email/dtos/EmailDTO.java
+++ src/main/java/com/ms/email/dtos/EmailDTO.java
@@ -2,7 +2,24 @@ package com.ms.email.dtos;
 
 import jakarta.validation.constraints.NotBlank;
 
-public record EmailDTO (@NotBlank (message = "The reference cannot be blanck") String ownerRef, String emailFrom, String emailTo, String subject, String text) {
+
+public record EmailDTO (
+        @NotBlank
+                (message = "The reference cannot be blanck")
+        String ownerRef,
+        @NotBlank
+                (message = "The email cannot be blanck")
+        String emailFrom,
+        @NotBlank
+                (message = "The email receiver cannot be blanck")
+        String emailTo,
+        @NotBlank
+                (message = "The subject cannot be blanck")
+        String subject,
+        @NotBlank
+                (message = "The email body cannot be blanck")
+        String text)
+{
 
 
 }
